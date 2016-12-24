b17aa6d1cdb3894d309ceac539dd529dd1c8ae52
arianadziedzic <ariana.dziedzic@gmail.com>
2016-12-24 11:36:11 -0500

    add App, Header, Inventory, and Order components

---
+++ catch-of-the-day/src/components/Order.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Order extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Order</p>
+      </div>
+    )
+  }
+}
+
+export default Order;
