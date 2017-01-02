775cbc84606813c6882c267d1ba68980e9d7160c
arianadziedzic <ariana.dziedzic@gmail.com>
2017-01-02 10:22:01 -0500

    add fish component and ordering functionality

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -14,7 +14,8 @@ class App extends React.Component {
       order: {}
     };
     this.addFish = this.addFish.bind(this);
-    this.loadSamples = this. loadSamples.bind(this);
+    this.loadSamples = this.loadSamples.bind(this);
+    this.addToOrder = this.addToOrder.bind(this);
   }
   addFish(fish) {
     // update state
@@ -31,6 +32,13 @@ class App extends React.Component {
       fishes: sampleFishes
     })
   }
+  addToOrder(key) {
+    const order = {...this.state.order};
+    // update or add the new number of fish ordered
+    order[key] = order[key] + 1 || 1;
+    // updates state
+    this.setState({ order });
+  }
   render() {
     return (
       <div className="catch-of-the-day">
@@ -40,11 +48,11 @@ class App extends React.Component {
             {
               Object
                 .keys(this.state.fishes)
-                .map(key => <Fish key={key} details={this.state.fishes[key]}/>)
+                .map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />)
             }
           </ul>
         </div>
-        <Order />
+        <Order fishes={this.state.fishes} order={this.state.order}/>
         <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
       </div>
     );
