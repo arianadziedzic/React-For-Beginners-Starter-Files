9580aaee1ca4652b2532505efce6c14f9f150c39
arianadziedzic <ariana.dziedzic@gmail.com>
2017-01-02 10:49:49 -0500

    add base and syncing to db

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyCwKdnJIsOuN7OI10QIuZrytoA04WVjcKY",
+    authDomain: "catch-of-day-6085b.firebaseapp.com",
+    databaseURL: "https://catch-of-day-6085b.firebaseio.com",
+});
+
+export default base;
--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,6 +4,7 @@ import Order from './Order';
 import Inventory from './Inventory';
 import sampleFishes from '../sample-fishes';
 import Fish from './Fish';
+import base from '../base';
 
 class App extends React.Component {
   constructor() {
@@ -13,9 +14,20 @@ class App extends React.Component {
       fishes: {},
       order: {}
     };
+
     this.addFish = this.addFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
     this.addToOrder = this.addToOrder.bind(this);
+  }
+  componentWillMount() {
+    this.ref = base.syncState(`${this.props.params.storeId}/fishes`, {
+      context: this,
+      state: 'fishes'
+    });
+  }
+
+  componentWillUnmount() {
+    base.removeBinding(this.ref);
   }
   addFish(fish) {
     // update state
