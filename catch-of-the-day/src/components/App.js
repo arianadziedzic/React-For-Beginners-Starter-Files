2959aca73935affb923499c1f2b378de511ed854
arianadziedzic <ariana.dziedzic@gmail.com>
2017-01-02 11:22:03 -0500

    add editing abilities to inventory component

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -18,6 +18,7 @@ class App extends React.Component {
     this.addFish = this.addFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
     this.addToOrder = this.addToOrder.bind(this);
+    this.updateFish = this.updateFish.bind(this);
   }
   componentWillMount() {
     // this runs right before the app is rendered
@@ -59,6 +60,11 @@ class App extends React.Component {
       fishes: sampleFishes
     })
   }
+  updateFish(key, updatedFish) {
+    const fishes = {...this.state.fishes};
+    fishes[key] = updatedFish;
+    this.setState({ fishes });
+  }
   addToOrder(key) {
     const order = {...this.state.order};
     // update or add the new number of fish ordered
@@ -84,7 +90,12 @@ class App extends React.Component {
           order={this.state.order}
           params={this.props.params}
         />
-        <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
+        <Inventory
+          addFish={this.addFish}
+          loadSamples={this.loadSamples}
+          fishes={this.state.fishes}
+          updateFish={this.updateFish}
+        />
       </div>
     );
   }
