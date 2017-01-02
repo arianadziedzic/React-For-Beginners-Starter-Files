613bc385a269d99ed720a7ad7cbca70ad03e5dad
arianadziedzic <ariana.dziedzic@gmail.com>
2017-01-02 10:35:02 -0500

    add order functionality to order component

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -2,6 +2,25 @@ import React from 'react';
 import { formatPrice } from '../helpers';
 
 class Order extends React.Component {
+  constructor() {
+    super();
+    this.renderOrder = this.renderOrder.bind(this);
+  }
+  renderOrder(key) {
+    const fish = this.props.fishes[key];
+    const count = this.props.order[key];
+
+    if(!fish || fish.status === 'unavailable') {
+      return <l1 key={key}>Sorry, {fish ? fish.name : 'fish'} is no longer available!</l1>
+    }
+
+    return (
+      <li key={key}>
+        <span>{count}lbs {fish.name}</span>
+        <span className="price">{(formatPrice(count * fish.price))}</span>
+      </li>
+    )
+  }
   render() {
     const orderIds = Object.keys(this.props.order);
     const total = orderIds.reduce((prevTotal, key) => {
@@ -17,12 +36,12 @@ class Order extends React.Component {
       <div className='order-wrap'>
         <h2>Your Order</h2>
         <ul className="order">
+          {orderIds.map(this.renderOrder)}
           <li className="total">
             <strong>Total:</strong>
             {formatPrice(total)}
           </li>
         </ul>
-
       </div>
     )
   }
